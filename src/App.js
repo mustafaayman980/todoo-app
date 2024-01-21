bd374462694efe27047ac17730c8fa78a43b33f7
mustafaayman980 <mustafaayman980@gmail.com>
2024-01-21 16:15:13 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+import { useRef, useState } from 'react';
+import './App.css';
+
+function App() {
+  const[todos, setodos]=useState([]);
+ const inputRef =useRef();
+const handelclick=()=>{
+  const text=inputRef.current.value;
+const newiteam = {complet:false,text};
+  setodos([...todos,newiteam])
+  inputRef.current.value="";
+}
+const handeldone=(index)=>{
+  const newtodos = [...todos];
+  newtodos[index].complet=!newtodos[index].complet;
+  setodos(newtodos);
+}
+
+const handldelete=(index)=>{
+  const newtodos = [...todos];
+  newtodos.splice(index,1);
+  setodos(newtodos);
+
+
+}
+
+
+  return (
+    <div className="App">
+      <h2>to do list</h2>
+      <div className='continer'>
+        <ul>
+        {todos.map(({text,complet },index)=>{
+          return <div className='item'>
+            <li key={index} onClick={()=>handeldone(index)} className={complet? "done" : "null"}> {text}</li>
+            <span className='trach' onClick={()=>handldelete(index)}>❌</span>
+
+          </div> 
+        })}
+      </ul>
+      <input ref={inputRef} placeholder='add iteam....'></input>
+      <button onClick={handelclick}>add</button>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
